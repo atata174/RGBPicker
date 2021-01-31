8629429a23ab18d5c376072ab352ab1a14ca627e
Albert Razhapov <razhapov_albert@icloud.com>
2021-01-31 04:15:33 +0300

    added first version code in comment. temporarily.

--- RGBPicker/ViewController.swift
+++ RGBPicker/ViewController.swift
@@ -58,3 +58,39 @@ class ViewController: UIViewController {
         return CGFloat(sliderValue)
     }
 }
+
+// Первая версия кода.
+
+/*
+ @IBOutlet var RGBView: UIView!
+ @IBOutlet var redColorCount: UILabel!
+ @IBOutlet var greenColorCount: UILabel!
+ @IBOutlet var blueColorCount: UILabel!
+ 
+ private var redColor: CGFloat = 0
+ private var greenColor: CGFloat = 0
+ private var blueColor: CGFloat = 0
+ 
+ override func viewDidLoad() {
+     super.viewDidLoad()
+     RGBView.layer.cornerRadius = 20
+ }
+ 
+ @IBAction func redColorSlider(_ sender: UISlider) {
+     redColor = CGFloat(sender.value)
+     redColorCount.text = String(round(100 * sender.value) / 100)
+     RGBView.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
+ }
+ 
+ @IBAction func greenColorSlider(_ sender: UISlider) {
+     greenColor = CGFloat(sender.value)
+     greenColorCount.text = String(round(100 * sender.value) / 100)
+     RGBView.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
+ }
+ 
+ @IBAction func blueColorSender(_ sender: UISlider) {
+     blueColor = CGFloat(sender.value)
+     blueColorCount.text = String(round(100 * sender.value) / 100)
+     RGBView.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
+ }
+ */
