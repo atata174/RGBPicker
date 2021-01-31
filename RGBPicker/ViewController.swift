e13504293d4b53b24085658a7ff8254ef8a38426
Albert Razhapov <razhapov_albert@icloud.com>
2021-01-31 04:04:54 +0300

    second version. looks like optimization

--- RGBPicker/ViewController.swift
+++ RGBPicker/ViewController.swift
@@ -14,31 +14,47 @@ class ViewController: UIViewController {
     @IBOutlet var greenColorCount: UILabel!
     @IBOutlet var blueColorCount: UILabel!
     
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     private var redColor: CGFloat = 0
     private var greenColor: CGFloat = 0
     private var blueColor: CGFloat = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        RGBView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
         RGBView.layer.cornerRadius = 20
+        
     }
     
-    @IBAction func redColorSlider(_ sender: UISlider) {
-        redColor = CGFloat(sender.value)
-        redColorCount.text = String(round(100 * sender.value) / 100)
-        RGBView.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
+    @IBAction func redColorSlider() {
+        redColor = sliderLogic(sliderValue: redSlider.value)
+        redColorCount.text = String(format: "%.2f", redSlider.value)
     }
     
-    @IBAction func greenColorSlider(_ sender: UISlider) {
-        greenColor = CGFloat(sender.value)
-        greenColorCount.text = String(round(100 * sender.value) / 100)
-        RGBView.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
+    @IBAction func greenColorSlider() {
+        greenColor = sliderLogic(sliderValue: greenSlider.value)
+        greenColorCount.text = String(format: "%.2f", greenSlider.value)
     }
     
-    @IBAction func blueColorSender(_ sender: UISlider) {
-        blueColor = CGFloat(sender.value)
-        blueColorCount.text = String(round(100 * sender.value) / 100)
-        RGBView.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
+    @IBAction func blueColorSlider() {
+        blueColor = sliderLogic(sliderValue: blueSlider.value)
+        blueColorCount.text = String(format: "%.2f", blueSlider.value)
+    }
+    
+    private func sliderLogic (sliderValue: Float) -> CGFloat {
+        RGBView.backgroundColor = UIColor(
+            red: redColor,
+            green: greenColor,
+            blue: blueColor,
+            alpha: 1)
+        return CGFloat(sliderValue)
     }
 }
-
