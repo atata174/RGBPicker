eea66ee920967f3c34cbd2649c6c824448c44653
Albert Razhapov <razhapov_albert@icloud.com>
2021-02-12 21:22:06 +0300

    removing extra code design editing optimization

--- RGBPicker/ViewController.swift
+++ RGBPicker/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var RGBView: UIView!
+    
     @IBOutlet var redColorCount: UILabel!
     @IBOutlet var greenColorCount: UILabel!
     @IBOutlet var blueColorCount: UILabel!
@@ -18,79 +19,35 @@ class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
-    private var redColor: CGFloat = 0
-    private var greenColor: CGFloat = 0
-    private var blueColor: CGFloat = 0
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        RGBView.layer.cornerRadius = 20
         
+        sliderLogic()
+        
+    }
+    
+    @IBAction func rgbAction(_ sender:UISlider) {
+        sliderLogic()
+        
+        switch sender.tag {
+        case 0: redColorCount.text = sliderLabelAction(slider: sender)
+        case 1: greenColorCount.text = sliderLabelAction(slider: sender)
+        case 2: blueColorCount.text = sliderLabelAction(slider: sender)
+        default: break
+        }
+    }
+    
+    private func sliderLogic () {
         RGBView.backgroundColor = UIColor(
             red: CGFloat(redSlider.value),
             green: CGFloat(greenSlider.value),
             blue: CGFloat(blueSlider.value),
             alpha: 1)
-        RGBView.layer.cornerRadius = 20
-        
     }
     
-    @IBAction func redColorSlider() {
-        redColor = sliderLogic(sliderValue: redSlider.value)
-        redColorCount.text = String(format: "%.2f", redSlider.value)
-    }
-    
-    @IBAction func greenColorSlider() {
-        greenColor = sliderLogic(sliderValue: greenSlider.value)
-        greenColorCount.text = String(format: "%.2f", greenSlider.value)
-    }
-    
-    @IBAction func blueColorSlider() {
-        blueColor = sliderLogic(sliderValue: blueSlider.value)
-        blueColorCount.text = String(format: "%.2f", blueSlider.value)
-    }
-    
-    private func sliderLogic (sliderValue: Float) -> CGFloat {
-        RGBView.backgroundColor = UIColor(
-            red: redColor,
-            green: greenColor,
-            blue: blueColor,
-            alpha: 1)
-        return CGFloat(sliderValue)
+    private func sliderLabelAction(slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
     }
 }
 
-// Первая версия кода.
-
-/*
- @IBOutlet var RGBView: UIView!
- @IBOutlet var redColorCount: UILabel!
- @IBOutlet var greenColorCount: UILabel!
- @IBOutlet var blueColorCount: UILabel!
- 
- private var redColor: CGFloat = 0
- private var greenColor: CGFloat = 0
- private var blueColor: CGFloat = 0
- 
- override func viewDidLoad() {
-     super.viewDidLoad()
-     RGBView.layer.cornerRadius = 20
- }
- 
- @IBAction func redColorSlider(_ sender: UISlider) {
-     redColor = CGFloat(sender.value)
-     redColorCount.text = String(round(100 * sender.value) / 100)
-     RGBView.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
- }
- 
- @IBAction func greenColorSlider(_ sender: UISlider) {
-     greenColor = CGFloat(sender.value)
-     greenColorCount.text = String(round(100 * sender.value) / 100)
-     RGBView.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
- }
- 
- @IBAction func blueColorSender(_ sender: UISlider) {
-     blueColor = CGFloat(sender.value)
-     blueColorCount.text = String(round(100 * sender.value) / 100)
-     RGBView.backgroundColor = UIColor(red: redColor, green: greenColor, blue: blueColor, alpha: 1)
- }
- */
